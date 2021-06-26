3105fb4f264b3d4f1640e64c57ce4e78752ca3af
Vincent Chao <vincentchao99@gmail.com>
2021-06-26 23:23:56 +0900

    Sort - add CharactersCollection

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,14 @@
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 import { Sorter } from './Sorter';
 
-const numbersCollection = new NumbersCollection([100, 3, -5, 10])
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([100, 3, -5, 10])
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(sorter.collection);
+
+
+const charactersCollection = new CharactersCollection('aXbyaa');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
 console.log(sorter.collection);
