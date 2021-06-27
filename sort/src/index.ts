5bc4850dbc473568b3c8e31cfac25d0cbfa02046
Vincent Chao <vincentchao99@gmail.com>
2021-06-27 15:46:03 +0900

    Sort - refactoring by using abstract class

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,24 +1,19 @@
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
-import { Sorter } from './Sorter';
 
-// const numbersCollection = new NumbersCollection([100, 3, -5, 10])
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(sorter.collection);
-
+const numbersCollection = new NumbersCollection([100, 3, -5, 10]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('aXbyaa');
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-console.log(sorter.collection);
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(100);
 linkedList.add(-10);
 linkedList.add(5);
 linkedList.add(10);
-const sorter1 = new Sorter(linkedList);
-sorter1.sort();
-console.log(linkedList.print());
+linkedList.sort();
+linkedList.print();
