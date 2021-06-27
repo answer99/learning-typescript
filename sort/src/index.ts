0a2d386da4deda539c629445aa50c238d3d2bf62
Vincent Chao <vincentchao99@gmail.com>
2021-06-27 12:02:39 +0900

    Sort - add LinkedList

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,5 +1,6 @@
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 import { Sorter } from './Sorter';
 
 // const numbersCollection = new NumbersCollection([100, 3, -5, 10])
@@ -12,3 +13,12 @@ const charactersCollection = new CharactersCollection('aXbyaa');
 const sorter = new Sorter(charactersCollection);
 sorter.sort();
 console.log(sorter.collection);
+
+const linkedList = new LinkedList();
+linkedList.add(100);
+linkedList.add(-10);
+linkedList.add(5);
+linkedList.add(10);
+const sorter1 = new Sorter(linkedList);
+sorter1.sort();
+console.log(linkedList.print());
