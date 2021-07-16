42d10a4739608dad86dbf15dbcae3169d2a59213
Vincent Chao <vincentchao99@gmail.com>
2021-07-16 12:16:06 +0900

    stats a way to reduce verbose when creating summary instance

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,13 +1,10 @@
 import { MatchReader  } from "./MatchReader";
 import { CsvFileReader } from "./CsvFileReader";
 import { Summary } from "./Summary";
-import { WinsAnalysis } from "./analyzers/WinsAnalysis";
-import { ConsoleReport } from "./reportTargets/ConsoleReport";
-import { HtmlReport } from "./reportTargets/HtmlReport";
 
 const dataReader = new CsvFileReader('football.csv')
 const reader = new MatchReader(dataReader);
 reader.load();
 
-const summary = new Summary(new WinsAnalysis('Fulham'), new HtmlReport())
+const summary = Summary.winsAnalysisWithHtmlReport('Huddersfield');
 summary.buildAndPrintReport(reader.matches)
