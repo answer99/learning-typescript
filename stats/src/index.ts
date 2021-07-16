42bd6dd39f23c44584495b4d3f4b82f577149654
Vincent Chao <vincentchao99@gmail.com>
2021-07-16 12:04:51 +0900

    stats Generating HTML report

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -3,10 +3,11 @@ import { CsvFileReader } from "./CsvFileReader";
 import { Summary } from "./Summary";
 import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { HtmlReport } from "./reportTargets/HtmlReport";
 
 const dataReader = new CsvFileReader('football.csv')
 const reader = new MatchReader(dataReader);
 reader.load();
 
-const summary = new Summary(new WinsAnalysis('Fulham'), new ConsoleReport())
+const summary = new Summary(new WinsAnalysis('Fulham'), new HtmlReport())
 summary.buildAndPrintReport(reader.matches)
