f9b925478e8e0105e9dafd01d6aab33c334cfd07
Vincent Chao <vincentchao99@gmail.com>
2021-07-22 18:40:44 +0900

    web framework final User refactor

---
+++ web/src/models/Model.ts
@@ -0,0 +1,67 @@
+import { AxiosPromise, AxiosResponse } from "axios";
+
+interface ModelAttributes<T> {
+  get<K extends keyof T>(key: K): T[K];
+  set(update: T): void;
+  getAll(): T;
+}
+
+interface Sync<T> {
+  fetch(id: number): AxiosPromise;
+  save(data: T): AxiosPromise;
+}
+
+interface Events {
+  on(eventName: string, callback: () => void): void;
+  trigger(eventName: string): void;
+}
+
+interface HasId {
+  id?: number;
+}
+
+export class Model<T extends HasId> {
+  constructor(
+    private attributes: ModelAttributes<T>,
+    private events: Events,
+    private sync: Sync<T>,
+  ){ }
+
+  get on() {
+    return this.events.on;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  set(update: T): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.attributes.get('id');
+    if(!id) {
+      return;
+    }
+    this.sync.fetch(id)
+      .then((res: AxiosResponse): void => {
+        // use this.set to trigger change event
+        this.set(res.data);
+      })
+  }
+
+  save(): void {
+    this.sync.save(this.attributes.getAll())
+      .then((res: AxiosResponse): void => {
+        this.trigger('save');
+      }).catch(() => {
+        this.trigger('error');
+      })
+  }
+}
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,7 +1,7 @@
-import { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
-import { Sync } from './Sync';
+import { ApiSync } from './ApiSync';
 import { Attributes } from './Attributes';
+import { Model } from './Model';
 
 interface UserProps {
   id?: number;
@@ -11,55 +11,12 @@ interface UserProps {
 
 const rootUrl = 'http://localhost:3000/users';
 
-export class User {
-  // Hard code dependencies
-  // (-) every time we create a user, we are going tot have the same
-  //     eventing class
-  // But now dont see any chance to repleace/change the eventing class
-  //
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attributes: Attributes<UserProps>;
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
-  }
-
-  get on() {
-    return this.events.on;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    this.events.trigger('change');
-  }
-
-  fetch(): void {
-    const id = this.attributes.get('id');
-    if(!id) {
-      return;
-    }
-    this.sync.fetch(id)
-      .then((res: AxiosResponse): void => {
-        // use this.set to trigger change event
-        this.set(res.data);
-      })
-  }
-
-  save(): void {
-    this.sync.save(this.attributes.getAll())
-      .then((res: AxiosResponse): void => {
-        this.trigger('save');
-      }).catch(() => {
-        this.trigger('error');
-      })
+export class User extends Model<UserProps>{
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
   }
 }
