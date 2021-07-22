6d370ce663fe38928fc3485f4d0143a00cd1f55b
Vincent Chao <vincentchao99@gmail.com>
2021-07-23 00:12:18 +0900

    web framework test User class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,13 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1, name: "name", age: 0 });
+const user = User.buildUser({ id: 1 });
 
 user.on('change', () => {
-  console.log("User was changed, update HTML DOM!");
-})
-
-user.on('save', () => {
   console.log(user);
 })
 
-user.save();
+user.fetch();
