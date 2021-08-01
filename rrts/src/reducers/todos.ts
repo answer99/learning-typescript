ea815669fc01d55c77ccdebd4cc654c4a4532bd3
Vincent Chao <vincentchao99@gmail.com>
2021-08-01 13:16:41 +0900

    rrts validating store structure

--- rrts/src/reducers/todos.ts
+++ rrts/src/reducers/todos.ts
@@ -1,7 +1,7 @@
 import { Todo, FetchTodosAction } from '../actions';
 import { ActionTypes } from '../actions/types';
 
-export const todoReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
   switch(action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
