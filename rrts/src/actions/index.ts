1d0ebdc878ad657ed1b869d0ce27d1b74239ffb7
Vincent Chao <vincentchao99@gmail.com>
2021-08-01 12:49:36 +0900

    rrts add more type safety to action 1. add interface for response.data 2. add enum for action type 3. add interface fet dispatch function

--- rrts/src/actions/index.ts
+++ rrts/src/actions/index.ts
@@ -1,20 +1,27 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
+import { ActionTypes } from './types'
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+interface FatchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
 
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get(url);
+    const response = await axios.get<Todo[]>(url);
 
-    dispatch({
-      type: 'FETCH_TODOS',
+    dispatch<FatchTodosAction>({
+      type: ActionTypes.fetchTodos,
       payload: response.data
     })
   }
 }
-
-// downside
-// 1. response.data is any type
-// 2. type is a harcodeed string(easy to make a type), enum is better
-// 3. dispatch is a generic function, we should use it
